ecab5c29cc891f4c3f4871106c92fe7606ca55fe
Logon27 <logondz27@gmail.com>
2022-08-23 22:01:47 -0400

    Fix Programming Proficiency Chart For Some Mobile Resolutions

--- portfolio/src/components/HomepageFeatures/index.js
+++ portfolio/src/components/HomepageFeatures/index.js
@@ -8,12 +8,14 @@ const FeatureList = [
     Svg: require('@site/static/img/programming-monitor.svg').default,
     description: (
       <>
+        <span className={styles.programmingListContainer}>
           <p className={styles.programmingList}>Java - <span className={styles.programmingDotFull}>⬤ ⬤ ⬤ ⬤ ⬤</span></p>
           <p className={styles.programmingList}>C - <span className={styles.programmingDotFull}>⬤ ⬤ ⬤ ⬤ ⬤</span></p>
           <p className={styles.programmingList}>Python - <span className={styles.programmingDotFull}>⬤ ⬤ ⬤ ⬤</span> ⬤</p>
           <p className={styles.programmingList}>SQL - <span className={styles.programmingDotFull}>⬤ ⬤ ⬤</span> ⬤ ⬤</p>
           <p className={styles.programmingList}>Typescript - <span className={styles.programmingDotFull}>⬤ ⬤</span> ⬤ ⬤ ⬤</p>
           <p className={styles.proficiencyNote}>(Proficiency ranked 1 to 5. Orange being filled)</p>
+        </span>
       </>
     ),
   },
